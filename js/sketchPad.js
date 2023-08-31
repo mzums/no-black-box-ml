4f75de52bf5d33d1d89cae52471b56b4fd1e5e99
mzums <annsie035@gmail.com>
2023-08-31 21:24:43 +0200

    drawing multiple lines

--- js/sketchPad.js
+++ js/sketchPad.js
@@ -1,5 +1,5 @@
 class SketchPad {
-    constructor(container, size=400) {
+    constructor(container, size=300) {
         this.canvas = document.createElement("canvas");
         this.canvas.width = size;
         this.canvas.height = size;
@@ -11,7 +11,7 @@ class SketchPad {
 
         this.ctx = this.canvas.getContext("2d");
 
-        this.path = [];
+        this.paths = [];
         this.isDrawing = false;
 
         this.#addEventListeners();
@@ -20,26 +20,37 @@ class SketchPad {
     #addEventListeners() {
         this.canvas.onmousedown = (evt) => {
             const mouse = this.#getMouse(evt);
-            this.path = [mouse];
+            this.paths.push([mouse]);
             this.isDrawing = true;
         }
         this.canvas.onmousemove = (evt) => {
             if (this.isDrawing) {
                 const mouse = this.#getMouse(evt);
-                this.path.push(mouse);
-                console.log(this.path.length);
+                const lastPath = this.paths[this.paths.length-1];
+                lastPath.push(mouse);
                 this.#redraw();
             }
         }
         this.canvas.onmouseup = () => {
             this.isDrawing = false;
         }
+        this.canvas.ontouchstart = (evt) => {
+            const loc = evt.touches[0];
+            this.canvas.onmousedown(ioc);
+        }
+        this.canvas.ontouchmove = (evt) => {
+            const loc = evt.touches[0];
+            this.canvas.onmousedown(ioc);
+        }
+        this.canvas.ontouchend = () => {
+            this.canvas.onmouseup();
+        }
     }
 
     #redraw() {
         this.ctx.clearRect(0, 0,
             this.canvas.width, this.canvas.height);
-        draw.path(this.ctx, this.path);
+        draw.paths(this.ctx, this.paths);
     }
 
     #getMouse = (evt) => {
