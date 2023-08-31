9960c8cdc557b19c10878f0b6e67cdbce6aeea18
mzums <annsie035@gmail.com>
2023-08-31 18:00:04 +0200

    refreshing after drawing one line

--- js/sketchPad.js
+++ js/sketchPad.js
@@ -11,17 +11,42 @@ class SketchPad {
 
         this.ctx = this.canvas.getContext("2d");
 
+        this.path = [];
+        this.isDrawing = false;
+
         this.#addEventListeners();
     }
 
     #addEventListeners() {
         this.canvas.onmousedown = (evt) => {
-            const rect = this.canvas.getBoundingClientRect();
-            const mouse = [
-                evt.clientX - rect.left,
-                evt.clientY - rect.top
-            ];
-            console.log(mouse);
+            const mouse = this.#getMouse(evt);
+            this.path = [mouse];
+            this.isDrawing = true;
         }
+        this.canvas.onmousemove = (evt) => {
+            if (this.isDrawing) {
+                const mouse = this.#getMouse(evt);
+                this.path.push(mouse);
+                console.log(this.path.length);
+                this.#redraw();
+            }
+        }
+        this.canvas.onmouseup = () => {
+            this.isDrawing = false;
+        }
+    }
+
+    #redraw() {
+        this.ctx.clearRect(0, 0,
+            this.canvas.width, this.canvas.height);
+        draw.path(this.ctx, this.path);
+    }
+
+    #getMouse = (evt) => {
+        const rect = this.canvas.getBoundingClientRect();
+        return [
+            Math.round(evt.clientX - rect.left),
+            Math.round(evt.clientY - rect.top)
+        ];
     }
 }
